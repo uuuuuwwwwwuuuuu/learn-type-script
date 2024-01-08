58b7a7cb50cbe93c508d6678abfa0e83df71f8fd
Denis <hopeelove2002@gmail.com>
2024-01-08 20:43:36 +0300

    info: types in ts

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+const revenue = 1000;
+const bonus = 500;
+const res = revenue + bonus;
+console.log(res);
--- app.ts
+++ app.ts
@@ -1,1 +1,5 @@
-const a = 1;
+const revenue: number = 1000;
+const bonus: number = 500;
+
+const res: number = revenue + bonus;
+console.log(res)
