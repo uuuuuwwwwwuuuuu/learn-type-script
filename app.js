7e857faad55e4ef2ba755db23cd9ea1d6dd7aeb3
Denis <hopeelove2002@gmail.com>
2024-01-09 09:42:48 +0300

    info: typles

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 "use strict";
-const arr = [1, 2, 3, 4, 5];
-console.log(arr.reduce((akb, element) => akb + element));
+const arr = [1, 'Dev', true, false, true, true];
+console.log(arr[4]);
--- app.ts
+++ app.ts
@@ -1,2 +1,3 @@
 const arr: [number, string, ...boolean[]] = [1, 'Dev', true, false, true, true];
 
+console.log(arr[4]);
