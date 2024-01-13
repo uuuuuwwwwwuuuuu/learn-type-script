885ea2fab73d24f39a08bee9073a0fb43fc5a5d3
Denis <hopeelove2002@gmail.com>
2024-01-13 09:21:44 +0300

    info: classes in TS

--- app.ts
+++ app.ts
@@ -1,44 +1,19 @@
-interface User {
-    name: string;
-    email: string;
-    login: string;
-}
+class User {
+    name: string
 
-interface Admin {
-    name: string;
-    role: number;
-}
-
-const user: User = {
-    name: "Вася",
-    email: "Vasiliy@gmail.com",
-    login: 'vasia'
-}
-
-function logId(id: string | number) {
-    if (isString(id)) {
-        console.log(id);
-    } else {
-        console.log(id);
+    constructor(name: string) {
+        this.name = name
     }
 }
 
-function isString(x: string | number): x is string {            //это bool
-    return typeof x === 'string';
+const user = new User('Вася');
+console.log(user);
+user.name = "Петя";
+console.log(user);
+
+class Admin {
+    role: number
 }
 
-function isAdmin(user: User | Admin): user is Admin {
-    return 'role' in user;
-}
-
-// function isAdmin(user: User | Admin): user is Admin {
-//     return (user as Admin).role !== undefined;
-// }
-
-function setRoleZero(user: User | Admin) {
-    if (isAdmin(user)) {
-        user.role = 0;
-    } else {
-        throw new Error('Пользовательно не админ');
-    }
-}
+const admin = new Admin();
+admin.role = 1;
