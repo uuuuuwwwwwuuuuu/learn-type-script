4d33cdbecf85cb8860dd89a97cac2f2789af4d30
Denis <hopeelove2002@gmail.com>
2024-01-15 10:26:42 +0300

    info: this

--- app.js
+++ app.js
@@ -1,58 +1,28 @@
 "use strict";
-class Product {
-    constructor(name, price) {
-        this.name = name;
-        this.price = price;
-    }
-}
-class Delivery {
-    constructor(date) { }
-}
-class HomeDelivery extends Delivery {
-    constructor(date, address) {
-        super(date);
-    }
-}
-class ShopDelivery extends Delivery {
-    constructor(shopId) {
-        super(new Date());
-    }
-}
-class Cart {
+class Payment {
     constructor() {
-        this.productList = [];
+        this.date = new Date();
+        this.getDateArrow = () => {
+            return this.date; //лучше юзать это, тогда с контекстом траблов нет
+        };
     }
-    setId() {
-        for (let i = 0; i < this.productList.length; i++) {
-            this.productList[i].id = i + 1;
-        }
-    }
-    addProduct(product) {
-        this.productList.push(product);
-        this.setId();
-    }
-    deleteProduct(id) {
-        this.productList.splice(id - 1, 1);
-        this.setId();
-    }
-    calcTotalPrice() {
-        return this.productList.reduce((akb, element) => akb + element.price, 0);
-    }
-    setDelivery(delivery) {
-        this.delivery = delivery;
-    }
-    checkout() {
-        if (this.productList.length !== 0 && this.delivery) {
-            return true;
-        }
-        else {
-            return false;
-        }
+    getDate() {
+        return this.date; //возвращает переменную date
     }
 }
-const cart = new Cart();
-cart.addProduct(new Product('Tomato', 10));
-cart.addProduct(new Product('Cucumber', 12));
-cart.addProduct(new Product('Bread', 3));
-console.log(cart.checkout());
-console.log(cart);
+const payment = new Payment();
+const user = {
+    id: 1,
+    paymentDate: payment.getDate //undefined, потому что getDate обращается к this, а в данном случаи this ссылается на объект user
+};
+// const user = {
+//     id: 1,
+//     paymentDate: payment.getDate.bind(payment)        //теперь всё работает корректно, так как мы указали на что должен ссылаться this
+// }
+// console.log(user.paymentDate())         //указывает на компайл ошибку так как не привязан контекст вызова
+class PaymentPersistent extends Payment {
+    save() {
+        return super.getDate();
+    }
+}
+console.log(new PaymentPersistent().save());
--- app.ts
+++ app.ts
@@ -1,17 +1,35 @@
-class UserService {
-    static db: any;
-    
-    create() {                              //доступнно для инстанса
-        UserService.db                      //обращение к статик в классе
+class Payment {
+    private date: Date = new Date();
+
+    getDate(this: Payment) {                //есть только на этапе TS и исключает возможность ошибки ниже
+        return this.date    //возвращает переменную date
     }
 
-    static async getUser(id: number) {            //не доступно для инстанса
-        return await UserService.db.findById(id);
-    }
-
-    static {
-        ///
+    getDateArrow = () => {
+        return this.date        //лучше юзать это, тогда с контекстом траблов нет
     }
 }
 
-UserService.db      //К static свойствам можно обращаться не создаая инстанс
+const payment = new Payment();
+
+const user = {
+    id: 1,
+    paymentDate: payment.getDate       //undefined, потому что getDate обращается к this, а в данном случаи this ссылается на объект user
+}
+
+// const user = {
+//     id: 1,
+//     paymentDate: payment.getDate.bind(payment)        //теперь всё работает корректно, так как мы указали на что должен ссылаться this
+// }
+
+// console.log(user.paymentDate())         //указывает на компайл ошибку так как не привязан контекст вызова
+
+
+class PaymentPersistent extends Payment {
+    save() {
+        // return super.getDateArrow();
+        return this.getDateArrow();          //если юзаю стрелочную функцию, то при наследованиии обращаться нужно к this, контекстом будет Payment
+    }
+}
+
+console.log(new PaymentPersistent().save());
