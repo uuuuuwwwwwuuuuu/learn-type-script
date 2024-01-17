efb1a85090c00c33c3ea63a5a53533e7a89bbb2b
Denis <hopeelove2002@gmail.com>
2024-01-17 14:12:16 +0300

    test: toString function

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 "use strict";
-function logMiddleWare(data) {
-    console.log(data);
-    return data;
+function toString(data) {
+    if (typeof data === 'object') {
+        return JSON.stringify(data);
+    }
+    return `${data}`;
 }
-const res = logMiddleWare(10);
-const arr = [3, 6, 7, 10, 5];
-const result = 4 in arr;
-console.log(result);
+console.log(toString({ name: 'Ivan', age: 30 }));
--- app.ts
+++ app.ts
@@ -1,12 +1,6 @@
-function logMiddleWare<T>(data: T): T {
-    console.log(data);
-    return data
+function toString<T>(data: T): string {
+    if (typeof data === 'object') {
+        return JSON.stringify(data);
+    }
+    return `${data}`;
 }
-
-const res = logMiddleWare<number>(10);
-
-function getSplitedHalf<T>(data: Array<T>): Array<T> {
-    const length = data.length / 2;
-    return data.splice(0, length);
-}
-
