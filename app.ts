3cfd451b9e73237796acc16149d836b7e9c2c497
Denis <hopeelove2002@gmail.com>
2024-01-17 14:18:08 +0300

    info: generics in interfaces

--- app.ts
+++ app.ts
@@ -1,6 +1,18 @@
-function toString<T>(data: T): string {
-    if (typeof data === 'object') {
-        return JSON.stringify(data);
+function getSplitHalf<T>(data: Array<T>): Array<T> {
+    const length = data.length / 2;
+    return data.splice(0, length);
+}
+
+const split: <T>(data: Array<T>) => Array<T> = getSplitHalf;
+
+interface ILogLine<T> {
+    timeStamp: Date;
+    data: T
+}
+
+const logLine: ILogLine<{a: number}> = {
+    timeStamp: new Date(),
+    data: {
+        a: 1
     }
-    return `${data}`;
 }
