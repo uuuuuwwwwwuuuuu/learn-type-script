084f309aa60da293338b3010fbbfff97cfe79263
Denis <hopeelove2002@gmail.com>
2024-01-17 15:56:53 +0300

    info: keyof

--- app.ts
+++ app.ts
@@ -1,1 +1,17 @@
-const a = 1;
+interface IUser {
+    name: string;
+    age: number;
+}
+
+type KeysOfUser = keyof IUser;
+
+function getValue<T, K extends keyof T>(obj: T, key: K) {
+    return obj[key];
+}
+
+const user: IUser = {
+    name: 'Вася',
+    age: 30
+}
+
+const userName = getValue(user, 'name');
