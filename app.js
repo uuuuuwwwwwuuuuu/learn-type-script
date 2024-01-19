760efc62e32ceacb03552baa4e8f3e862d273643
Denis <hopeelove2002@gmail.com>
2024-01-19 12:56:44 +0300

    info: decorators in real proj

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 "use strict";
-class UserService {
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+let UserService = class UserService {
     constructor() {
         this.users = 1000;
     }
     getUsersInDataBase() {
         return this.users;
     }
+};
+UserService = __decorate([
+    threeUserAdvanced
+], UserService);
+function nullUser(target) {
+    target.prototype.users = 0;
 }
-function nullUser(obj) {
-    obj.users = 0;
-    return obj;
+function threeUserAdvanced(constructor) {
+    return class extends constructor {
+        constructor() {
+            super(...arguments);
+            this.users = 3;
+        }
+    };
 }
 console.log(new UserService().getUsersInDataBase());
-console.log(nullUser(new UserService()).getUsersInDataBase());
--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ interface IUserService {
     getUsersInDataBase(): number
 }
 
+@threeUserAdvanced
 class UserService implements IUserService {
     users: number = 1000;
 
@@ -11,15 +12,14 @@ class UserService implements IUserService {
     }
 }
 
-function nullUser(obj: IUserService) {
-    obj.users = 0
-    return obj;
+function nullUser(target: Function) {           //не работает
+    target.prototype.users = 0
 }
 
-function logUser(obj: IUserService) {
-    console.log("Users: " + obj.users)
-    return obj;
+function threeUserAdvanced<T extends { new(...args: any[]): {}}>(constructor: T) {
+    return class extends constructor {
+        users = 3;
+    }
 }
 
-console.log(new UserService().getUsersInDataBase());
-console.log(nullUser(new UserService()).getUsersInDataBase());
+console.log(new UserService().getUsersInDataBase());            //Выводит 3
