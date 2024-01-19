64674cb62235009fa497f8951bfb05a18829bcfe
Denis <hopeelove2002@gmail.com>
2024-01-19 23:04:29 +0300

    info: factory pattrn

--- app.ts
+++ app.ts
@@ -1,1 +1,98 @@
-const a = 1;
+interface IInsurance {
+    id: number;
+    status: string;
+    setVehicle(vehicle: any) : void;
+    submit(): Promise<boolean>;
+}
+
+class TFInsurance implements IInsurance {
+    id: number;
+    status: string;
+    private vehicle: any;
+
+    setVehicle(vehicle: any): void {
+        this.vehicle = vehicle;
+    }
+
+    async submit(): Promise<boolean> {
+        const res = await fetch('', 
+        {
+            method: 'POST', 
+            body: JSON.stringify({ vehicle: this.vehicle})
+        });
+
+        const data = await res.json();
+        return data.isSuccess;
+    }
+}
+
+class ABInsurance implements IInsurance {
+    id: number;
+    status: string;
+    private vehicle: any;
+
+    setVehicle(vehicle: any): void {
+        this.vehicle = vehicle;
+    }
+
+    async submit(): Promise<boolean> {
+        const res = await fetch('', 
+        {
+            method: 'POST', 
+            body: JSON.stringify({ vehicle: this.vehicle})
+        });
+
+        const data = await res.json();
+        return data.yes;
+    }
+}
+
+abstract class InsuranceFactory {
+    db: any;
+    abstract createInsurance(): IInsurance;
+
+    saveHistory(ins: IInsurance) {
+        this.db.save(ins.id, ins.status);
+    }
+}
+
+class TFInsuranceFactory extends InsuranceFactory {
+    createInsurance(): TFInsurance {
+        return new TFInsurance();
+    }
+}
+
+class ABInsuranceFactory extends InsuranceFactory {
+    createInsurance(): ABInsurance {
+        return new ABInsurance();
+    }
+}
+
+const tfInsuranceFactory = new TFInsuranceFactory();
+const ins = tfInsuranceFactory.createInsurance();
+tfInsuranceFactory.saveHistory(ins);
+
+//_________________________________   Ниже лучше
+
+const INSURANCE_TYPE = {
+    tf: TFInsurance,
+    ab: ABInsurance
+};
+
+type IT = typeof INSURANCE_TYPE;
+
+class InsuranceFactoryAlt {
+    db: any;
+
+    createInsurance<T extends keyof IT>(type: T): IT[T] {
+        return INSURANCE_TYPE[type];
+    }
+
+    saveHistory(ins: IInsurance) {
+        this.db.save(ins.id, ins.status)
+    }
+}
+
+const insuranceFactoryAlt = new InsuranceFactoryAlt();
+const instans1 = new (insuranceFactoryAlt.createInsurance('tf'));
+insuranceFactoryAlt.saveHistory(instans1);
