90dce7b0854306beff55b02bced9f3a737242c2d
Denis <hopeelove2002@gmail.com>
2024-01-20 09:13:31 +0300

    info: singleton pattern

--- app.js
+++ app.js
@@ -1,38 +1,31 @@
+"use strict";
 class MyMap {
-    private static instance: MyMap;
-
-    map: Map<number, string> = new Map();
-
-    private constructor() { }
-
-    clean(): void {
+    constructor() {
         this.map = new Map();
     }
-
-    public static get(): MyMap {
+    clean() {
+        this.map = new Map();
+    }
+    static get() {
         if (!MyMap.instance) {
             MyMap.instance = new MyMap();
         }
         return MyMap.instance;
     }
 }
-
 class Service1 {
-    addMap(key: number, value: string) {
+    addMap(key, value) {
         const myMap = MyMap.get();
         myMap.map.set(key, value);
     }
 }
-
 class Service2 {
-    getKeys(key: number) {
+    getKeys(key) {
         const myMap = MyMap.get();
         return myMap.map.get(key);
     }
 }
-
 const service1 = new Service1();
 const service2 = new Service2();
-
 service1.addMap(1, 'something');
 console.log(service2.getKeys(1));
